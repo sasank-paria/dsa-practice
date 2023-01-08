7b92b359faef74bd23413d8a7a4fc5b15a986cdb
paria <83511327+sasank-paria@users.noreply.github.com>
2023-01-08 14:26:59 +0530

    .

--- 2.pattern problems/program3.cpp
+++ 2.pattern problems/program3.cpp
@@ -9,6 +9,17 @@
 using namespace std;
 
 int main(){
-    
+    int i=1 , j=1 , n ;
+    cout<<"enter a nubmer upto which wanna pattern"<<endl;
+    cin>>n;
+    while(i<=n){
+        while(j<=n){
+            cout<<j;
+            j++;
+        }
+        cout<<endl;
+        i++;
+        j=1;
+    }
     return 0;
 }
---
+++ 2.pattern problems/program4.cpp
@@ -0,0 +1,13 @@
+// pattern:
+// 4 3 2 1
+// 4 3 2 1
+// 4 3 2 1
+// 4 3 2 1
+
+#include<iostream>
+using namespace std;
+
+int main(){
+       
+    return 0;
+}
